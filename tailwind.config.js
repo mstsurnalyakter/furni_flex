1e24ac309363354ee457966589cc8ad15874229c
mstsurnalyakter <surnalyms@gmail.com>
2024-09-05 19:28:49 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: '"Inter", sans-serif',
+        barlow: '"Barlow", sans-serif',
+      },
+    },
+  },
+  plugins: [],
+});
